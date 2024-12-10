177360dd1cae5e3b843f1e2acf737dd07a77aea6
Alicia P <146850705+alipbar@users.noreply.github.com>
2024-12-09 20:39:59 -0600

    Primer commit
    Se agregaron los primeros archivos.

---
+++ main.js
@@ -0,0 +1,17 @@
+var app = document.getElementById('app');
+
+var typewriter = new Typewriter(app, {
+  loop: true
+});
+//Comentario
+typewriter.typeString('Desarrolladora Frontend Jr.')
+  .pauseFor(2500)
+  .deleteAll()
+  .typeString('¡Si lo puedes imaginar, lo puedes crear')
+  .pauseFor(2500)
+
+  //caracteres que se borrarán
+  .deleteChars(6)
+  .typeString('<strong> Programar!</strong>')
+  .pauseFor(2500)
+  .start();
